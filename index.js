820249410bb8140c3c67ae66aff49c61fa387faa
danielmurphy1 <daniel.murphy.1@gmail.com>
2023-09-11 15:39:47 -0400

    completed

--- index.js
+++ index.js
@@ -22,11 +22,36 @@
 function checkForBingo (bingoCard, drawnNumbers) {
   // this code for debug purposes, you can remove.
   console.log('Drawn Numbers: ' + JSON.stringify(drawnNumbers));
+  const winningArrays = [
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19],
+    [20, 21, 22, 23, 24],
+    [0, 5, 10, 15, 20],
+    [1, 6, 11, 16, 21],
+    [2, 7, 12, 17, 22],
+    [3, 8, 13, 18, 23],
+    [4, 9, 14, 19, 24],
+    [0, 6, 12, 18, 24],
+    [4, 8, 12, 16, 20]
+  ];
 
-  for (let i=0, len=bingoCard.length; i<len; i++) {
-    let row = Math.floor(i/5);
-    let col = i % 5;
-   //  console.log(`${row},${col}: ${bingoCard[i]}`);
+  const checkWin = (numbersInput, array) => {
+    let counter = 0;
+    for (const index of array) {
+      if (numbersInput.includes(bingoCard[index]) || bingoCard[index] === "FREE") {
+        counter++;
+      }
+    }
+    return counter === 5;
+  }
+  
+  
+  for (const array of winningArrays) {
+    if(checkWin(drawnNumbers, array)){
+      return true;
+    }
   }
 
   return false;
@@ -37,7 +62,7 @@ module.exports = checkForBingo;
 // here are some samples
 
 // this should return true with diagonal + free
-checkForBingo(
+const check1 = checkForBingo(
   [
     8, 29, 35, 54, 65,
     13, 24, 44, 48, 67,
@@ -49,9 +74,25 @@ checkForBingo(
     8, 24, 53, 72
   ]
 );
+console.log("return value of checkForBingo1", check1)
+
+// this should return true with row
+const check2 = checkForBingo(
+  [
+    8, 29, 35, 54, 65,
+    13, 24, 44, 48, 67,
+    9, 21, 'FREE', 59, 63,
+    7, 19, 34, 53, 61,
+    1, 20, 33, 46, 72
+  ],
+  [
+    8, 29, 35, 54, 65, 73,78
+  ]
+);
+console.log('return value of checkForBingo2', check2)
 
 // this should return false
-checkForBingo(
+const check3 = checkForBingo(
   [
    8, 29, 35, 54, 65,
    13, 24, 44, 48, 67,
@@ -63,3 +104,19 @@ checkForBingo(
     1, 33, 53, 65, 29, 75
   ]
 );
+console.log('return value of checkForBingo3', check3)
+
+//return true column with free
+const check4 = checkForBingo(
+  [
+   8, 29, 35, 54, 65,
+   13, 24, 44, 48, 67,
+   9, 21, 'FREE', 59, 63,
+   7, 19, 34, 53, 61,
+   1, 20, 33, 46, 72
+  ],
+  [
+    35, 19, 44, 72, 56, 34, 65, 33
+  ]
+);
+console.log('return value of checkForBingo4', check4)
